0d855fe536923345df9d59f4ceae49454f82c949
Chipotle0914 <kantitus33@gmail.com>
2025-08-04 11:19:11 -0400

    datset had wrong label name, question and figtht both process fairly ok, haven't run into edge cases yet

--- limbus_md_app.py
+++ limbus_md_app.py
@@ -9,7 +9,7 @@ import keyboard
 import sys
 
 # Load YOLOv5 model
-model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5/runs/train/mirror_dungeon_train7/weights/best.pt')
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5/runs/train/mirror_dungeon_train8/weights/best.pt')
 model.conf = 0.7 
 
 # Initialize EasyOCR reader once
@@ -35,10 +35,13 @@ CONFIRM_REGION = (0.0854, 0.5741, 0.9547, 0.9194)
 SELECT_REGION = (0.7646, 0.7972, 0.9943, 0.9889)
 REWARD_REGION = (0.0745, 0.1028, 0.8000, 0.2472)
 P_ENTER_REGION = (0.6245, 0.6491, 0.9307, 0.8935)
+SKIP_REGION = (0.3802, 0.1731, 0.5266, 0.7037)
+COMMENCE_CONTINUE_PROCEED_REGION = (0.7438, 0.7713, 0.9927, 0.9667)
+SKILL_CHECK_REGION = (0.0042, 0.8185, 0.7849, 0.8759)
 
 DEFAULT_PRIOR = [REWARD_STAR, REWARD_MONEY, REWARD_GAMBLE, REWARD_RANDOM, REWARD_RESOURCE]
 #move cursor to targeted class and click
-def yolo_detect_click(target_class: str, click_num: int = 1):
+def yolo_detect_click(target_class: str, click_num: int = 1, top_most: bool = False):
     print(f"🔍 Searching for '{target_class}' using YOLO...")
     
     # Capture screenshot
@@ -56,8 +59,22 @@ def yolo_detect_click(target_class: str, click_num: int = 1):
         print(f"❌ '{target_class}' not found.")
         return False
 
+    ####DEBUG
+    if target_class == ACQUIRE_EGO:
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        debug_img_path = f"debug_acquire_ego_{timestamp}.png"
+        cv2.imwrite(debug_img_path, cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR))
+        print(f"🖼️ Saved debug screenshot to {debug_img_path}")
+    ####DEBUG
+    # Get target box
+    if top_most:
+        match = match.sort_values(by='ymin').iloc[0]
+        print("↕️ Top-most mode enabled")
+    else:
+        match = match.iloc[0]
+
     # Get first matching box
-    x1, y1, x2, y2 = match.iloc[0][['xmin', 'ymin', 'xmax', 'ymax']]
+    x1, y1, x2, y2 = match[['xmin', 'ymin', 'xmax', 'ymax']]
     center_x = int((x1 + x2) / 2)
     center_y = int((y1 + y2) / 2)
 
@@ -68,7 +85,6 @@ def yolo_detect_click(target_class: str, click_num: int = 1):
     #click for click_num
     for _ in range(click_num):
         pyautogui.click()
-        print("I clicked!")
         time.sleep(0.2)
     print(f"🖱️ Clicked on '{target_class}'")
 
@@ -102,8 +118,10 @@ def scan_box_click_text(target, region, moveTo_flag = 1,click_flag = 1):
             if moveTo_flag:
                 pyautogui.moveTo(center_x, center_y, duration=0.2)
             
-            if click_flag:
+            for _ in range(click_flag):
                 pyautogui.click()
+                time.sleep(0.2)
+                print(f"🖱️ Clicked on '{target}'")
             return True
 
     print(f"❌ '{target}' not found in region.")
@@ -132,6 +150,34 @@ def check_reward():
 
 def check_p_enter():
     return scan_box_click_text("win", P_ENTER_REGION, 0, 0) or scan_box_click_text("damage", P_ENTER_REGION, 0, 0)
+
+def click_skip_5_times():
+    return scan_box_click_text("skip", SKIP_REGION, 1, 5)
+
+def click_continue():
+    return scan_box_click_text("continue", COMMENCE_CONTINUE_PROCEED_REGION)
+
+def click_commence():
+    return scan_box_click_text("commence", COMMENCE_CONTINUE_PROCEED_REGION)
+
+def click_proceed():
+    return scan_box_click_text("proceed", COMMENCE_CONTINUE_PROCEED_REGION)
+
+def check_skil_check():
+    for best_skill in ["very high", "high", "normal", "low", "very low"]:
+        found = scan_box_click_text(best_skill, SKILL_CHECK_REGION, 0, 0)
+        if found:
+            print(f"🎯 Skill check level detected: {best_skill}")
+            return True 
+    return False
+
+def click_best_skill_check():
+    for best_skill in ["very high", "high", "normal", "low", "very low"]:
+        found = scan_box_click_text(best_skill, SKILL_CHECK_REGION)
+        if found:
+            print(f"🎯 Skill check level clicked: {best_skill}")
+            return True 
+    sys.exit("❌ No skill check to click on screen.")
 
 def click_reward_prior():
     for target in DEFAULT_PRIOR:
@@ -169,21 +215,27 @@ def process_fight():
     while True:
         #Scen1: go back to train
         if yolo_detect_click(TRAIN, 0):
-            time.sleep(1.5)
+            time.sleep(1)
             if yolo_detect_click(TRAIN, 0):
                 break
             else:
                 continue
         #Scen2: pick reward choice
         elif check_reward():
+            #wait for cards to load
+            time.sleep(1.5)
             #choose based on priority
             if click_reward_prior():
-                time.sleep(0.5)
+                time.sleep(1)
                 click_confirm()
                 time.sleep(3)
                 continue
             else:
-                sys.exit("REWARD_CHOICE Error")
+                #the 33 percent where we still are on the reward screen, but we need to click confirm
+                if check_confirm():
+                    click_confirm()
+                    time.sleep(1)
+                    continue
         #Scen3: accept ego gift by select + confirm
         elif yolo_detect_click(ACQUIRE_EGO):
             if click_select():
@@ -201,7 +253,7 @@ def process_fight():
         elif check_confirm():
             if not(check_reward()):
                 click_confirm()
-                time.sleep(2)
+                time.sleep(1)
                 continue
 
             
@@ -214,11 +266,61 @@ def process_fight():
             else:
                 continue
 
+def process_question():
+    #enter qustion
+    while True:
+        #Scen1: go back to train      
+        if yolo_detect_click(TRAIN, 0):
+            time.sleep(1.5)
+            if yolo_detect_click(TRAIN, 0):
+                break
+            else:
+                continue
+        #Scen3: click confirm for ego gift after the question stage
+        elif check_confirm():
+            click_confirm()
+            time.sleep(1)
+            continue
+        # click proceed
+        elif click_proceed():
+            click_skip_5_times()
+            time.sleep(1.5)
+            continue
+        #click commence
+        elif click_commence():
+            time.sleep(3)
+            click_skip_5_times()
+            time.sleep(1.5)
+            continue
+        #click continue(continue is always the last step)
+        elif click_continue():
+            time.sleep(1)
+            continue
+        #Scen2: check A choices first
+        elif yolo_detect_click(A_CHOICES, top_most=True):
+            click_skip_5_times()
+            time.sleep(1)
+            continue
+        #Scen2: check B choices Second
+        elif yolo_detect_click(B_CHOICES, top_most=True):
+            click_skip_5_times()
+            time.sleep(1)
+            continue
+       
+        #skill check
+        elif check_skil_check():
+            click_best_skill_check()
+            time.sleep(1)
+            continue
+        else:
+            click_skip_5_times()
+            time.sleep(1)
 
 
 def test_features():
     click_enter()
     time.sleep(2)
+    #process_question()
     process_fight()
 if __name__ == "__main__":
     time.sleep(2)
